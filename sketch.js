25fa91cf5f28f1fad9edccd46af59845b4361de7
shivambaisoya07 <113031138+shivambaisoya07@users.noreply.github.com>
2022-09-28 21:30:30 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,15 +1,11 @@
-
-var PLAY = 1;
-var END = 0;
-var gameState = PLAY;
-var bow , arrow,  background, redB, pinkB, greenB ,blueB ,arrowGroup;
+var bow , arrow,  scene;
 var bowImage, arrowImage, green_balloonImage, red_balloonImage, pink_balloonImage ,blue_balloonImage, backgroundImage;
+var select_balloon=1
+var score
 
-var score =0;
 function preload(){
   
   backgroundImage = loadImage("background0.png");
-  
   arrowImage = loadImage("arrow0.png");
   bowImage = loadImage("bow0.png");
   red_balloonImage = loadImage("red_balloon0.png");
@@ -28,109 +24,108 @@ function setup() {
   scene = createSprite(0,0,400,400);
   scene.addImage(backgroundImage);
   scene.scale = 2.5
+
+  score=0;
   
   // creating bow to shoot arrow
   bow = createSprite(380,220,20,50);
   bow.addImage(bowImage); 
   bow.scale = 1;
   
-   score = 0  
- //redB= new Group();
- 
- // arrowGroup= new Group();
-
-  
+   score = 0    
 }
 
 function draw() {
  background(0);
- 
 
- 
- if(gameState === PLAY)
- {
-      /*Uncomment correct option 
-        according to PLAY state*/  
-      // // moving ground
-       scene.velocityX = -3 
-      // //destroy bow
-      // bow.destroy();
-      // //reset the background
-       if (scene.x < 0){
-           scene.x = scene.width/2;
-          }
-      // //moving bow
-       bow.y = World.mouseY      
-      // //stop background movement
-      // scene.velocityX = 0;
+  
 
+
+  // moving ground
+    scene.velocityX = -3 
+
+    if (scene.x < 0){
+      scene.x = scene.width/2;
+    }
+
+  //moving bow
+  bow.y = World.mouseY
   
    // release arrow when space key is pressed
   if (keyDown("space")) {
-    createArrow();  
+    createArrow();
+    
   }
+   
+  //Uncomment correct option to get random number from 1 to 4 
+    select_balloon = Math.round(random(1,4));
+   // select_balloon = random(1,4);
+   // select_balloon = Math.round(random());
+   // select_balloon = Math.round(random(1,4,2));
   
-  //creating continous enemies
-  var select_balloon = Math.round(random(1,4));
-  
-  if (World.frameCount % 100 == 0) {
-    switch(select_balloon ){
-      case 1: redBalloon();
-      break;
-      case 2:blueBalloon();
-      break;
-      case 3:pinkBalloon();
-      break;
-      case 4:greenBalloon();
-      break;
-      default:break;
-    }
-  }
- }
+   if (World.frameCount % 100 == 0) {
+
+    //uncomment the correct switch statement
+
+            // switch(select_balloon ){
+            // case 1: redBalloon();
+            // case 2:blueBalloon();
+            // case 3:pinkBalloon();
+            // case 4:greenBalloon();
+            // default:break;
+            // }
 
 
-  if (gameState === END) {
-    /*Uncomment correct option 
-      according to END state*/  
-      // // moving ground
-      // scene.velocityX = -3 
-      // //destroy bow
-       bow.destroy();
-      // //reset the background
-      // if (scene.x < 0){
-      //     scene.x = scene.width/2;
-      //    }
-      // //moving bow
-      // bow.y = World.mouseY      
-      // //stop background movement
-       scene.velocityX = 0;
+             switch(select_balloon ){
+             case 1: redBalloon();
+             break;
+             case 2:blueBalloon();
+             break;
+             case 3:pinkBalloon();
+             break;
+             case 4:greenBalloon();
+             break;
+             default:break;
+             }
 
-  }
 
-if (frameCount>1000) {
-  //red.destroyEach();
-  gameState=END; 
+            // switch(select_balloon ){
+            // case 1: redBalloon();
+            // break;
+            // case 1:blueBalloon();
+            // break;
+            // case 1:pinkBalloon();
+            // break;
+            // case 1:greenBalloon();
+            // break;
+            // default:break;
+            // }
+
 }
-
-
-
-
- 
   
+
+
+
   drawSprites();
-  text("Score: "+ score, 300,50);
+
+  textSize(24);
+  fill("red");
+  stroke("black");
+  text("score:"+ score,270,30);
+  
 }
 
 
-
-
-
-
-
-
-
-
-
+// Creating  arrows for bow
+ function createArrow() {
+  var arrow= createSprite(100, 100, 60, 10);
+  arrow.addImage(arrowImage);
+  arrow.x = 360;
+  arrow.y=bow.y;
+  arrow.velocityX = -4;
+  arrow.lifetime = 100;
+  arrow.scale = 0.3;
+}
 
 function redBalloon() {
   var red = createSprite(0,Math.round(random(20, 370)), 10, 10);
@@ -138,7 +133,6 @@ function redBalloon() {
   red.velocityX = 3;
   red.lifetime = 150;
   red.scale = 0.1;
-//  redB.add(red);
 }
 
 function blueBalloon() {
@@ -163,18 +157,4 @@ function pinkBalloon() {
   pink.velocityX = 3;
   pink.lifetime = 150;
   pink.scale = 1
-
-}
-
-// Creating  arrows for bow
- function createArrow() {
-  var arrow= createSprite(100, 100, 60, 10);
-  arrow.addImage(arrowImage);
-  arrow.x = 360;
-  arrow.y=bow.y;
-  arrow.velocityX = -4;
-  arrow.lifetime = 100;
-  arrow.scale = 0.3;
-//  arrowGroup.add(arrow);
-
 }
